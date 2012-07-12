9abccd11518c5a4d3d7d82f90d9ac68584e86a08
Yurlungur <DrLoco3000@gmail.com>
2012-07-11 19:13:21 -0700

    new file:   #dnd_classes.py# 	new file:   #party_manager.py# 	deleted:    .#README 	modified:   characters.py 	modified:   dnd_classes.py 	modified:   party_manager.py

--- characters.py
+++ characters.py
@@ -18,7 +18,36 @@ paulo.read_from_dict(legacy.paulo)
 emily = dnd_classes.character()
 emily.read_from_dict(legacy.emily)
 
+# Initialize new characters
+# Ali
+ali = dnd_classes.character()
+ali.name = "Ali"
+ali.STR = -1
+ali.DEX = 3
+ali.CON = 1
+ali.INT = -1
+ali.WIS = 1
+ali.CHA = 2
+ali.BA = 1
+ali.GRA = -1
+ali.INITIATIVE = 3
+ali.AC = {'FULL':15,
+          'TOUCH':13,
+          'FLAT-FOOTED':12}
+ali.SAVES = {'FORT':1,
+             'REF':3,
+             'WILL':0}
+ali.ATTACKS = {'short sword':[[0],[1,6,0],[19,2]],
+               'short bow':[[4],[1,6,0],[20,3]],
+               'FULL'::[[0],[1,6,0],[19,2]]}
+ali.SKILLS = {'CLIMB':1,
+              'CRAFT MAKING ARROWS':0,
+              'DIPLOMACY':4,
+              'DISABLE DEVICE':2,
+              'ESCAPE ARTIST':7,
+              'GATHER INFORMATION':6}
+
 # Character name dictionary. Used in party class initialization:
-character_list = [isaac,paulo,emily]
+character_list = [isaac,paulo,emily,ali].sort()
 
 
